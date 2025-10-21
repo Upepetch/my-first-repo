3e9911481b74dfcd357e28901886764a0fa9eded
Upepetch <h.mmendes@hotmail.com>
2025-10-21 17:58:58 +0100

    Add files via upload

---
+++ website/array-script.js
@@ -0,0 +1,97 @@
+/*
+
+// Array of numerical values
+let numbers = [1, 2, 3, 4, 5];
+
+// Array of strings
+let fruits = ['apple', 'banana', 'orange', 'grape'];
+
+// Array of mixed data types
+let mixedArray = [1, 'hello', true, null, {name: 'John'}, [1, 2, 3]];
+
+// Array of objects
+let students = [
+  { name: 'john', age: 20 },
+  { name: 'Jane', age: 22 },
+  { name: 'Mark', age: 21 },
+
+];
+
+// Array of other arrays the we call a matrix.
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+// Array of other arrays with different lengths.
+let nestedArray = [
+  ['a', 'b', 'c'],
+  ['d', 'e'],
+  ['f', 'g', 'h', 'i'],
+];
+
+// An empty array
+let emptyArray = [];
+
+*/
+
+
+// Array Indexing
+/*
+  const books = [
+    'Lord of the Rings', 
+    'To Kill a Mockingbird',
+    '1984',
+    'The Alchemist', 
+    'The Great Gatsby'
+  ];
+console.log(books);
+
+console.log(books[4]);
+
+*/
+
+
+// Array Methods
+
+  const books = [
+    'Lord of the Rings', 
+    'To Kill a Mockingbird',
+    '1984',
+    'The Alchemist', 
+    'The Great Gatsby'
+  ];
+
+/*
+  // For adding another value(length)
+books[books.length] = 'A vida e bela';
+console.log(books);
+
+*/
+
+  // Another way of adding an item to the end of an array.
+
+  books.push('The In The Night Garden');
+ // console.log(books);
+
+
+ // For removing the last item
+
+     //books.pop();
+
+ // For adding or removing multiple items.
+
+    //books.splice(); // books.splice(6) => removes every item that comes before what is in brackets.
+
+// Method for removing the first item of the Array.
+
+    //books.shift('', '');
+
+// The oposite to add the items to the beginning.
+
+   //books.unshift('', '');
+
+
+
+
